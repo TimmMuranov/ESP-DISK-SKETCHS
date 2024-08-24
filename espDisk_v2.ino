8e7f5030d563ba84373d0bd82a538d4d76895fa1
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-08-24 21:21:27 +0300

    Update espDisk_v2.ino
    Украсил кнопки и добавил кнопку "Creat", которая создает пустой текст. Пока что нет кнопок сохранения текста а только чтение.

--- espDisk_v2.ino
+++ espDisk_v2.ino
@@ -5,6 +5,8 @@
 #include <SD.h>
 #include <FS.h>
 
+#define MAX_BUFFER 1024
+
 const char *ssid = "TimsServer"; // Имя вашей Wi-Fi сети
 const char *password = "12345678"; // Пароль от вашей Wi-Fi сети
 
@@ -16,6 +18,7 @@ String nameList = "";
 String butText = "";
 
 void buildNameList() {
+    butText ="";
     while (true) {
         File entry = root.openNextFile();
         if (!entry) break; 
@@ -29,10 +32,25 @@ void buildNameList() {
     Serial.println(nameList);
 
   String textNameMenue = 
-  "<!DOCTYPE html><html><head><title>Title</title>" 
+  "<!DOCTYPE html><html><head><title>Title</title><meta charset=\"UTF-8\">" 
   "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head>" 
   "<body>"
-  "<style>.butStyle {width: 50%;}</style>";//создаем заголовок html файла
+  "<style>.butStyle {background-color: #888; width:40%;margin-bottom: 10px;} .butStyleX {width: 100%;}"
+  " * {padding: 0;margin: 0;box-sizing: border-box;}"
+  ".wrapper{width: 100%;height: 100vh;display: flex;align-items: center;justify-content: center;background: #fff; margin-bottom: 15px;}"
+  ".btn {position: relative;text-decoration: none;color: #fff;text-transform: uppercase;font-size: 13px;"
+  "width: 40%;min-height: 28px;padding: 5px 0px;display: flex;align-items: center;justify-content: center;" //параметры всей кнопки
+  "font-family: \"Micra Normal\";text-align: center;background-color: rgba(24, 27, 38, 0.8);overflow: hidden;"
+  "border-radius: 2px;border: 1px solid #0f3d5b;clip-path: polygon(0% 36%,13% 0%,100% 0%,100% 0%,100% 65%,87.5% 100%,0% 100%,0% 100% );}"//параметры обрезаний
+  ".btn:before, .btn:after, .btn > span:nth-of-type(2):before, .btn > span:nth-of-type(2):after {"
+  "content: "";position: absolute;background: #0f3d5b;} .btn:before, .btn:after {width: calc(100% - 12px);height: 1px;display: none;}"
+  ".btn:before {top: 0;left: 12px;position: absolute;} .btn:after {bottom: 0;left: 12px;}"
+  ".btn > span {position: absolute;top: 0;left: 0;width: 100%;height: 100%;}"
+  ".btn > span:nth-of-type(2):before {height: 26px;width: 1px; -webkit-transform: rotate(54deg); transform: rotate(49deg);}"
+  ".btn > span:nth-of-type(2):after {height: 26px;width: 1px; -webkit-transform: rotate(45deg);transform: rotate(49deg);}"
+  ".btn > span:nth-of-type(2):before {top: -6.3px;left: 1.8px;}"
+  ".btn > span:nth-of-type(2):after {bottom: -6.3px;right: 1.8px;}"
+  "</style>";//создаем заголовок html файла
   
   String fileName = "";
   int butNumber = 0;
@@ -62,6 +80,8 @@ void buildNameList() {
     Serial.println("no files");
   }
 
+  textNameMenue += "<div class=\"btn\" ><a href=\"/textWin\" class=\"btn\" id='creatButton'>Creat</a></div>";
+
   textNameMenue += "<script>";
 
   textNameMenue +=
@@ -86,23 +106,41 @@ void buildNameList() {
 }
 
 void catchFile() {
-
+    
     butText = server.arg("buttonText");
     Serial.println("opened: " + butText);
 }
 
 void openTextWindow(){
+    
+    File file = SD.open(butText);
+    if (!file) {
+        Serial.println("Failed to open file");
+    }
+    String fileData = "";
+    int avail = file.available();
+    uint8_t buffer[MAX_BUFFER];
+    while (avail > 0) {
+        if (avail > MAX_BUFFER) avail = MAX_BUFFER;
+        file.read(buffer, avail);
+        for (int i = 0; i < avail; ++i) {
+            fileData += (char)buffer[i];
+        }
+        avail = file.available();
+    }
+    file.close();
+  
   String textWindow =  
-  "<!DOCTYPE html><html><head><title>Title</title>" 
-  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head>" 
-  "<body>"
-  "<style>.butStyle {width: 100%;}</style>";
+  "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'>" 
+  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+  "<title>Title</title></head><body>"
+  "<style>.butStyle {width: 100%;} .textarea{width: 100vw; height: 50vh; resize: none;}</style>";
 
-  textWindow += "open window with: " + butText;
+  textWindow += "Start edit: " + butText;
 
-   //чтение документа с одноименным именем и отправка его на сторону клиента
+  textWindow += "<textarea id='story' class = 'textarea' placeholder='To write...'>" + fileData + "</textarea>";
 
-    textWindow += "<button class='butStyle' onclick=\"location.href='/'\">Back to home</button>";
+    textWindow += "<button class='butStyle' onclick=\"location.href='/'\">Back to home</button><p>";
     textWindow += "<script></script>";
     textWindow += "</body></html>";
     
@@ -111,23 +149,20 @@ void openTextWindow(){
 
 void setup() {
   Serial.begin(9600);
-//=============================================================
+
   while(!Serial){} // Ждём инициализации работы с шиной UART
   Serial.print("Initializing SD card...");
   if(!SD.begin(4)){Serial.println("initialization failed!"); return;}
   Serial.println("initialization done.");
   root = SD.open("/");
   Serial.println("done!");
-//=============================================================
 
     WiFi.mode(WIFI_AP); // Установка режима точки доступа
     WiFi.softAP(ssid, password); // Создание точки доступа с указанным именем и паролем
-    
     IPAddress ip(192, 168, 1, 1); // IP-адрес точки доступа
     IPAddress gateway(192, 168, 1, 1); // Шлюз по умолчанию
     IPAddress subnet(255, 255, 255, 0); // Маска подсети
     WiFi.softAPConfig(ip, gateway, subnet); // Настройка параметров точки доступа
-
     // Регистрация обработчика корневого пути
     server.on("/", buildNameList);
     server.on("/load", catchFile);
