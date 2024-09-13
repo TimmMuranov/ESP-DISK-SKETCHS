598c83737ff432d36fb3e672bd3a07b55c924cd3
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-13 11:43:10 +0300

    Обновил fList.js
    Добавил в функцию отправку названия кнопки на сервер

--- espDisk/data/fList.js
+++ espDisk/data/fList.js
@@ -1,5 +1,23 @@
 function dirText(event) {
     const buttonText = event.target.textContent;
-    alert('Вы нажали кнопку с текстом: ' + buttonText);
+    console.log('Вы нажали кнопку с текстом: ' + buttonText);
     /* отправка запроса на сервер*/
+    fetch('/list', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+      body: JSON.stringify({
+      data: buttonText
+      })
+   })
+    .then(res => res.json()).then(
+        data => {
+          console.log(data);
+          }
+       )
+    .catch(error => {
+        console.error('Error:', error);
+        }
+    );
 }
