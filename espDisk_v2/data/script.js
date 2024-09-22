e74658c3e4da94cae5687370658e539b27e691b9
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-23 01:48:01 +0300

    Update script.js
    Теперь на каждую из кнопок (создать удалить загрузить) есть свой обработчик. Далее нужно добавить универсальный обработчик для кнопок-файлов.

--- espDisk_v2/data/script.js
+++ espDisk_v2/data/script.js
@@ -1,12 +1,11 @@
-const textArea = document.getElementById('textArea');
-  const inputArea = document.getElementById('inputArea');
-  const submitButton = document.getElementById('submitButton');
-  textArea.value = '>';
-  submitButton.addEventListener('click', async () => {
-    const data = inputArea.value;
-    inputArea.value = '';
+const inputArea = document.getElementById('inputArea');
+const submitButton = document.getElementById('submitButton');
+const creatF = document.getElementById('creatFile');
+const creatD = document.getElementById('creatDir');
 
-    try {
+submitButton.addEventListener('click', async () => {
+const data = inputArea.value;
+try {
         // Отправляем данные на сервер и сохраняем ответ
         const response = await fetch('/q', {
             method: 'POST',
@@ -20,71 +19,57 @@ const textArea = document.getElementById('textArea');
 
         // Проверяем, был ли запрос успешным
         if (!response.ok) {
-            textArea.value += 'Ошибка сервера: ' + response.statusText + '\n';
+            console.log('Ошибка сервера: ' + response.statusText);
             return;
         }
 
         // Получаем ответ от сервера
         const serverResponseText = await response.text();
-
+        alert(serverResponseText);
 //------------- добавление отловленных команд ---------------
-        if (data === 'clear') {
-            textArea.value = '>';
-            return;
-        }
-        else if (data === 'upload'){
-            upload();
-        }
-        textArea.value += serverResponseText;
-
-
-        textArea.scrollTop = textArea.scrollHeight;
-
     } catch (error) {
         console.error('Error sending data: ', error);
-        textArea.value += 'Ошибка отправки данных: ' + error + '\n';
     }
 });
 
-document.addEventListener('keyup', event => {
-if (event.code === 'Enter') {
-  submitButton.click();
-  inputArea.focus();
-  }
+creatF.addEventListener('click', async () => {
+const data = "creatFile";
+try {
+        const response = await fetch('/bf', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data })
+        });
+        if (!response.ok) {
+            console.log('Ошибка сервера: ' + response.statusText);
+            return;
+        }
+        const serverResponseText = await response.text();
+        alert(serverResponseText);
+    } catch (error) {
+        console.error('Error sending data: ', error);
+    }
 });
 
-function upload() {
-    // Создаем элемент input файлового поля
-    var fileInput = document.createElement("input");
-    fileInput.type = 'file';
-
-    fileInput.click();
-
-    fileInput.addEventListener('change', function () {
-        // Проверяем, что файл выбран
-        if (fileInput.files.length > 0) {
-            var formData = new FormData();
-
-            // Добавление выбранного файла к объекту FormData
-            formData.append('file', fileInput.files[0]);
-
-            // Отправка файла на сервер с использованием fetch API
-            fetch('/f', {
-                method: 'POST',
-                body: formData // Здесь не нужно указывать заголовки для Content-Type
-            })
-            .then(response => {
-                // Проверка на успешный ответ
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                // Пытаемся преобразовать ответ в JSON
-                return response.json();
-            })
-            .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
-        } else {
-            console.error('No file selected');
+creatD.addEventListener('click', async () => {
+const data = "creatDir";
+try {
+        const response = await fetch('/bd', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data })
+        });
+        if (!response.ok) {
+            console.log('Ошибка сервера: ' + response.statusText);
+            return;
         }
-    });
-}
+        const serverResponseText = await response.text();
+        alert(serverResponseText);
+    } catch (error) {
+        console.error('Error sending data: ', error);
+    }
+});
