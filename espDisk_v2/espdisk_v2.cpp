924de0ded82adaff3388243d5c12bb848878b3e9
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-22 22:11:34 +0300

    Update espdisk_v2.cpp
    добавил пару строк

--- espDisk_v2/espdisk_v2.cpp
+++ espDisk_v2/espdisk_v2.cpp
@@ -7,7 +7,24 @@
 #include <FS.h>
 
 int main (){
+  Serial.begin(115200);
 
+    while(!Serial){}//ждем serial
+
+    Serial.print("Initializing SD card...");
+    if(!SD.begin(4)) Serial.println("initialization failed!");
+    Serial.println("initialization SD card done.");
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+
+    server.on("/", winOpen);
+    server.on("/q", HTTP_POST, handleData);
+    server.begin();
+    Serial.println("Access Point started");
+
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
 
   while(1){
     
