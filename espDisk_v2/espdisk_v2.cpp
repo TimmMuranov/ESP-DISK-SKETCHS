eb0590515d73aef8e3dc707f312dfb095a902696
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-22 21:03:53 +0300

    Update espdisk_v2.cpp
    добавил библиотеки

--- espDisk_v2/espdisk_v2.cpp
+++ espDisk_v2/espdisk_v2.cpp
@@ -1,1 +1,17 @@
 #include <arduino.h>
+#include "ArduinoJson.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <SPI.h>
+#include <SD.h>
+#include <FS.h>
+
+int main (){
+
+
+  while(1){
+    
+  }
+
+return 0;
+}
